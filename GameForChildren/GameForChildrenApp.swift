430dc2c11cf633d3ce6f5a314c33325c59681e02
Andrii Kyrychenko <akyrych84@gmail.com>
2022-07-29 12:34:15 -0400

    Initial Commit

---
+++ GameForChildren/GameForChildrenApp.swift
@@ -0,0 +1,17 @@
+//
+//  GameForChildrenApp.swift
+//  GameForChildren
+//
+//  Created by Andrii Kyrychenko on 29/07/2022.
+//
+
+import SwiftUI
+
+@main
+struct GameForChildrenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
